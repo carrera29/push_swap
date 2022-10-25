8fb94e0bc82cf24b89262396f25e542286a9f600
carrera29 <99269629+carrera29@users.noreply.github.com>
2022-10-25 17:55:27 +0200

    Add files via upload

---
+++ include/push_swap.h
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chloeplatt <chloeplatt@student.42.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/13 09:30:41 by clcarre           #+#    #+#             */
+/*   Updated: 2022/10/25 15:53:34 by chloeplatt       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+
+typedef struct node
+{
+	int			x;
+	int			p;
+	struct node	*next;
+	struct node	*prev;
+}	t_node;
+
+typedef struct push_swap
+{
+	t_node	*list_a;
+	t_node	*list_b;
+	t_node	*curr;
+	t_node	*aux;
+	t_node	*last;
+	t_node	*last_b;
+	char	**number;
+	int		unsorted;
+	int		i;
+	int		n;
+	int		x;
+	int		d;
+	int		steps;
+	int		rot;
+	int		rev;
+	int		min;
+	int		max;
+	int		mid;
+	int		mp;
+}			t_push;
+
+//		utils
+void	free_node(t_push *p, t_node **list_a, t_node **list_b);
+void	error_msg(t_push *p, t_node **list_a, t_node **list_b);
+int		ft_atoi(t_push *p, const char *str);
+
+//		split
+char	**ft_split(char const *s, char c);
+
+//		check_in
+void	simplify_num(int i, t_node **list);
+int		check_numbers(t_push *p, t_node **list);
+void	insert_beginning_node(t_node **list, int value, int position);
+void	insert_end_node(t_node **list, int value, int position);
+void	check_in(t_push *p, char **argv);
+
+//		move_numbers
+void	swap(t_node **list, int i);
+void	rotate(t_node **list, t_node **last, int i);
+void	reverse_rotate(t_node **list, t_node **last, int i);
+void	push(t_node **pusher, t_node **to_list, int i);
+
+//		long_stack
+int		is_shortest_b(t_push *p, t_node **list, t_node **last);
+int		is_shortest_a(t_push *p, int max, t_node **list, t_node **last);
+void	push_stack_b(t_push *p, t_node **list_a, t_node **list_b);
+void	sort_stack_a(t_push *p, t_node **list_a, t_node **list_b);
+void	long_stack(t_push *p, t_node **list_a, t_node **list_b);
+
+#endif
--- makefile
+++ makefile
@@ -6,37 +6,47 @@
 #    By: chloeplatt <chloeplatt@student.42.fr>      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/07/13 13:09:12 by clcarre           #+#    #+#              #
-#    Updated: 2022/10/24 15:20:47 by chloeplatt       ###   ########.fr        #
+#    Updated: 2022/10/25 17:49:02 by chloeplatt       ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-NAME  	= 	push_swap.a
-CFLAGS 	= 	-Wall -Wextra -Werror
+NAME  	= $(BIN_DIR)/push_swap.a
+SRC_DIR	= ./src
+OBJ_DIR	= ./obj
+INC_DIR	= ./include
+BIN_DIR	= ./bin
+SRC		= 	$(SRC_DIR)/push_swap.c \
+			$(SRC_DIR)/check_in.c \
+			$(SRC_DIR)/ft_split.c \
+			$(SRC_DIR)/long_stack.c \
+			$(SRC_DIR)/move_numbers.c \
+			$(SRC_DIR)/utils.c		
+OBJ 	= 	$(SRC:.c=.o)
 
-OBJ_PAT =	./obj/
-SRC		= 	./src/push_swap.c \
-			./src/check_in.c \
-			./src/ft_split.c \
-			./src/long_stack.c \
-			./src/move_numbers.c \
-			./src/utils.c
+# -I incluye la ruta del *.h
+CFLAGS 	= 	-Wall -Wextra -Werror -I$(INC_DIR)
 
-OBJS 	= 	$(SRC:.c=.o)
+$(NAME) : $(OBJ)
+	mkdir -p $(BIN_DIR)
+	ar -rcs $(NAME) $(OBJ) 
+
+%.o : %.c
+	gcc -c $(CFLAGS) $< -o $@
+	gcc $(CFLAGS) $(SRC) -o push_swap
+# -include $(OBJ_DIR)/*.d
 
 all : $(NAME)
 
-$(NAME) : $(OBJS)
-	ar -rcs $(NAME) $(OBJS)
-
-.c.o :
-	gcc $(CFLAGS) -c $< -o $(<:.c=.o)
-	
 clean : 
-	rm -f $(OBJS)
+	rm -f $(OBJ)
+# rm -r $(OBJ_DIR)
 
 fclean : clean
 	rm -f $(NAME)
+	rm -r $(BIN_DIR)
+	rm -f push_swap
 
 re : fclean all
 
+# indica que son acciones, no archivos que deban generarse
 .PHONY : all clean fclean re
